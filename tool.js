a816738b0797804dbfba3e6778ef75654f214695
ManHiu <94962460+kunhtml@users.noreply.github.com>
2023-02-23 00:15:51 +0700

    Update tool.js

--- tool.js
+++ tool.js
@@ -2,18 +2,12 @@
 // @name         Bypass ChatGPT
 // @namespace    https://www.facebook.com/m.a.n.h.h.ieeu
 // @version      0.8.1.0.1
-// @description  bypass chatGPT khi quá tải
+// @description  bypass chatGPT khi quá tải | Mua ChatGPT liên hệ fb.com/m.a.n.h.h.ieeu 
 // @author       Max Stewie
 // @match        https://chat.openai.com/auth/login
 // @icon         https://seeklogo.com/images/C/chatgpt-logo-02AFA704B5-seeklogo.com.png
 // @grant        none
 // ==/UserScript==
 
-(() => {
-  const content = document.body.innerHTML;
-  if(!content.includes("Welcome to ChatGPT") && !content.includes("checking")) {
-    location.reload();
-    return;
-  }
-  console.log("Mua ChatGPT liên hệ fb.com/m.a.n.h.h.ieeu ");
-})();
+
+eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('(()=>{24 1=7.8.9;10(!1.4("11 12 2")&&!1.4("6")){14.15();13}16.17("18 2 19ê3 0ệ 20.21/22.23.3.0.0.5 ")})();',10,25,'h|content|ChatGPT|n|includes|ieeu|checking|document|body|innerHTML|if|Welcome|to|return|location|reload|console|log|Mua|li|fb|com|m|a|const'.split('|')))
